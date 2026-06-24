7a09cb0a9905e97b95528e069f787984569e1418
Krish Kumar <krishkumar.work3@gmail.com>
2026-06-24 11:37:32 +0530

    add helper for creating Redis consumer groups

--- examples/c05-groups.rs
+++ examples/c05-groups.rs
@@ -150,3 +150,37 @@ async fn run_consumer(
 
 	Ok(reader_handle)
 }
+
+async fn create_group(
+	client: &Client,
+	stream_name: &str,
+	group_name: &str,
+) -> Result<(), Box<dyn std::error::Error>> {
+
+	let mut con =
+		client.get_multiplexed_async_connection().await?;
+
+	let group_create_res: Result<(), _> =
+		con.xgroup_create_mkstream(
+			stream_name,
+			group_name,
+			"0"
+		)
+		.await;
+
+	if let Err(err) = group_create_res {
+		if let Some("BUSYGROUP") = err.code() {
+			println!(
+				"XGROUP - group '{group_name}' ALREADY created"
+			);
+		} else {
+			return Err(err.into());
+		}
+	} else {
+		println!(
+			"XGROUP - group '{group_name}' created"
+		);
+	}
+
+	Ok(())
+}
